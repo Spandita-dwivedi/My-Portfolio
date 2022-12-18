1322d81a3e573f23fdb079de8325590ef2f8549f
spandita dwivedi <spandita.dwi@gmail.com>
2022-12-18 12:05:54 +0530

    Typography and background cover

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -5,17 +5,25 @@ import styles from "./Main.module.scss"
 export default function Maincomp() {
     return (
         <>
-<div className={styles.bg}>
-<video autoPlay muted loop style={{ width: '100%', backgroundSize:'cover'}}>
-        <source src="/video/my.mp4" />
-      </video>
-    
-    <div className={styles.overlay}>
+            <div className={styles.bg}>
+                <video autoPlay muted loop style={{ width: '100%', backgroundSize: 'cover' }}>
+                    <source src="/video/my.mp4" />
+                </video>
 
-        <div className={styles.head}>Hi</div>
-    </div>
-    
-  </div>
-  </>
+                <div className={styles.overlay}>
+
+                    <div className={styles.head}>
+                        <h1 className={styles.head1}>
+                            HI,
+                            <br />I'M SPANDITA
+                        </h1>
+                        <p className={styles.head2}>A coder who loves to design.</p>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </>
     )
 }
