6301d377b2b4725013ab157ca0a527f2c8eed033
spandita dwivedi <spandita.dwi@gmail.com>
2022-12-19 07:28:07 +0530

    About me

---
+++ components/About/About.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styles from "./About.module.scss"
+import { CiLocationOn } from 'react-icons/ci';
+import { SlGraduation } from 'react-icons/sl';
+
+
+export default function Maincomp() {
+    return (
+        <>
+            <div className={styles.main}>
+                <div className={styles.container}>
+
+
+                    <div className={styles.box}>
+                        <h1 className={styles.head}>IT'S ME</h1>
+                        <p className={styles.head1}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae aperiam commodi repellendus dolor dolores quibusdam nihil vero laudantium, libero, sed animi sequi asperiores explicabo blanditiis aliquam magnam sit aspernatur reprehenderit architecto maiores eveniet. Fugit, accusamus cupiditate. Dolores, aut tenetur voluptate excepturi pariatur ea quaerat esse nulla vero corporis necessitatibus delectus eligendi, obcaecati, sed provident magni voluptatem ducimus quos veritatis. Exercitationem.</p>
+                    </div>
+                    <div className={styles.image}>
+                        <img className={styles.img} src="/images/img.jpg"></img>
+                    </div>
+
+
+
+                </div>
+                <div className={styles.iconnav}>  <div className={styles.list}>
+                    <div className={styles.icon} ><CiLocationOn />  India </div>
+
+
+                    <div className={styles.icon}><SlGraduation /> Madhav Institute Of Technology & Science   </div>
+
+
+
+                </div></div>
+
+
+
+
+
+
+
+            </div>
+
+
+
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,15 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import Maincomp from '../components/Main/Main'
 import Navbarcomp from '../components/Navbar/Navbar'
+import Aboutcomp from '../components/About/About'
 
 export default function Home() {
   return (
     <>
     <Navbarcomp />
    <Maincomp/>
+   <Aboutcomp/>
+   
    </>
   )
 }
