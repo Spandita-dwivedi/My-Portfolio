e06a641e44ce2e3396f2445d73d25731c8d0a922
spandita dwivedi <spandita.dwi@gmail.com>
2023-01-24 22:44:28 +0530

    cards fixed and vertical cards added

--- components/Vertical/Vertical.jsx
+++ components/Vertical/Vertical.jsx
@@ -8,8 +8,9 @@ export default function Verticalcomp() {
         <>
 
             <div className={styles.cards}>
+                <img className={styles.back} src='/images/texture.jpg' />
 
-                <div className={styles.card1}>
+                {/* <div className={styles.card1}>
 
 
                     <img className={styles.img1} src='/images/graphic-design.png' />
@@ -34,6 +35,8 @@ export default function Verticalcomp() {
 
                 </div>
 
+                
+
                 <div className={styles.card2}>
                     <img className={styles.img1} src='/images/web-programming.png' />
                     <h1 className={styles.head2}> Frontend Developer</h1>
@@ -54,10 +57,88 @@ export default function Verticalcomp() {
 
 
                 </div>
-            
+                */}
+
+
+                <div className={styles.card1}>
+
+                    <h1 className={styles.head} style={{ fontWeight: '800' }}>DESIGNER</h1>
+                    <p>I value simple content structure, clean design patterns, and thoughtful interactions.</p>
+
+
+                    <h4 style={{ fontWeight: 'Bolder' }}>Things I enjoy designing :</h4>
+                    <p>UI, Web, Apps, Logo</p>
+
+                    <h4 style={{ fontWeight: 'Bolder' }}>Design Tools :</h4>
+
+                    <div style={{ fontWeight: '300' }} className={styles.list}>
+
+                        <li>Figma</li>
+                        <li>Font awesome</li>
+                        <li>Balsamiq</li>
+                        <li>Framer</li>
+                        <li>And ofcourse pen & paper</li>
+                    </div>
+
+
+
+                </div>
+
+                <div className={styles.card12}>
+                    <img className={styles.design} src='/images/design.jpg' />
+                </div>
+
+
+
 
             </div>
-            <hr className={styles.hrrr} style={{ width: '30%', marginLeft: '34vw' ,marginTop: '5vw' , borderBottom:'none'}} />
+
+
+
+
+            <div className={styles.cardss}>
+                <img className={styles.backs} src='/images/texture.jpg' />
+
+
+                
+
+                    <div className={styles.card12s}>
+                        <img className={styles.prog} src='/images/prog.jpg' />
+                    </div>
+                    <div className={styles.card1s}>
+                    <h1 className={styles.heads} style={{ fontWeight: '800' }}>FRONTEND DEVELOPER</h1>
+                    <p>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
+
+
+                    <h4 style={{ fontWeight: 'Bolder' }}>Frameworks i use :</h4>
+                    <p>Express.js, React, Next.js, Node.js</p>
+
+                    <h4 style={{ fontWeight: 'Bolder' }}>Dev Tools :</h4>
+
+                    <div style={{ fontWeight: '300' }} className={styles.list}>
+
+                        <li>VS code</li>
+                        <li>Vercel</li>
+                        <li>Github</li>
+                        <li>Bootstrap</li>
+                        <li>Mantine</li>
+                    </div>
+
+
+
+                </div>
+
+
+
+
+
+
+            </div>
+
+
+
+
+
 
 
         </>
