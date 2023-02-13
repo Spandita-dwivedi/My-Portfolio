2689abebe4d5b5b11d406b2f0629d1155cfefd08
spandita dwivedi <spandita.dwi@gmail.com>
2023-02-13 14:47:39 +0530

    Document instead of next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import Head from 'next/head'
+
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import { Html, Head, Main, NextScript } from 'next/document'
+
 import Maincomp from '../components/Main/Main'
 import Navbarcomp from '../components/Navbar/Navbar'
 import Aboutcomp from '../components/About/About'
@@ -18,8 +20,8 @@ export default function Home() {
     <meta name="keywords" content="spandita"/>
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
   </Head>
-  <script  async  src="https://unpkg.com/aos@next/dist/aos.js"></script>
-  <script async>
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
     AOS.init();
   </script>
     <Navbarcomp />
