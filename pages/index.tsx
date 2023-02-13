19b5344b2b929875dbcfe176bdb9823a7de1d0f4
spandita dwivedi <spandita.dwi@gmail.com>
2023-02-13 14:51:18 +0530

    Err

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
-import { Html, Head, Main, NextScript } from 'next/document'
+
 
 import Maincomp from '../components/Main/Main'
 import Navbarcomp from '../components/Navbar/Navbar'
@@ -20,10 +21,10 @@ export default function Home() {
     <meta name="keywords" content="spandita"/>
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
   </Head>
-  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-  <script>
+  <Script src="https://unpkg.com/aos@next/dist/aos.js"></Script>
+  <Script>
     AOS.init();
-  </script>
+  </Script>
     <Navbarcomp />
    <Maincomp/>
    <Aboutcomp/>
