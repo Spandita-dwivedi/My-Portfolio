d3147e56665ee8b46f19aa371cb46df000c48c0b
spandita dwivedi <spandita.dwi@gmail.com>
2023-03-09 01:20:18 +0530

    Fixed about issues

--- components/Me/Me.jsx
+++ components/Me/Me.jsx
@@ -12,12 +12,14 @@ export default function Mecomp(){
 
         <div className={styles.cont2}>
 <h1 className={styles.spandita}>I'm spandita</h1>
-<p className={styles.spandita1}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod perspiciatis nam architecto provident odit libero, magni voluptas dolorum praesentium veritatis ab, harum officia quae illum quisquam aspernatur. Nihil impedit quo iure vitae ipsa? Repudiandae laudantium excepturi odit aspernatur, mollitia error!</p>
+<p className={styles.spandita1}>I'm a student, currently conferring my B.Tech in Artificial Intelligence and Data Science.
+
+When I created my first website, I still remember the amount of delight I felt. I love to create meaningful exposures through my designs and my developing skills. Playing guitar while singing is something I am extremely grateful for. love to read and plant. Always open to new opportunities and eager to learn something new everyday.</p>
 
         </div>
 
         <div className={styles.cont3}>
-            <img className={styles.photu} src='/images/img3.jpeg'/>
+            <img className={styles.photu} src='/images/img-1.jpg'/>
 
         </div>
 
